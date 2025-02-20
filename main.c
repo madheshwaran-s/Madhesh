3e3e0331a4950740cb7fe0ec75a166f055a5f374
madheshwaran-s <madheshwaran.s@yantravision.com>
2025-02-20 15:15:09 +0530

    Added sub fuction

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 
-int add( int a, int b) { return a+b; }
+int add(int a, int b) { return a + b; }
+int sub(int a, int b) { return a - b; }
 
 int main(){
-   printf("Added value: %d\n", add(5, 2));
+   printf("Addition: %d\n", add(5, 2));
+   printf("Subtraction: %d\n", sub(5, 2));
    return 0;
 }
