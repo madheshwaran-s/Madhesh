2e5ec1655dc477cdc8cd1d35bf17685dc2a51464
https://github.com/iniyanas-12 <iniyanas@yantravision.com>
2025-02-20 15:26:58 +0530

    included div()

--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 int add(int a, int b) { return a + b; }
 int sub(int a, int b) { return a - b; }
 int mul(int a, int b) { return a * b; }
+int div(int a, int b) { return a / b; }
 int main(){
    printf("Added value: %d\n", add(5, 2));
    printf("Subtraction: %d\n", sub(5, 2));
    printf("Multiplication: %d\n", mul(3, 4));
+   printf("Division: %d\n", div(10,2));
    return 0;
 }
