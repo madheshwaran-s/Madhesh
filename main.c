5f4057507fde2835fbe7bfed1c3aca0e7f599ef6
athul-yv <athul.krishna@yantravision.com>
2025-02-20 15:23:32 +0530

    included multi()

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 
 int add(int a, int b) { return a + b; }
 int sub(int a, int b) { return a - b; }
+int mul(int a, int b) { return a * b; }
 int main(){
    printf("Added value: %d\n", add(5, 2));
    printf("Subtraction: %d\n", sub(5, 2));
+   printf("Multiplication: %d\n", mul(3, 4));
    return 0;
 }
