c75d8d4ad37b7630b4a50b32f2bd36eafbab0aa6
https://github.com/iniyanas-12 <iniyanas@yantravision.com>
2025-02-27 22:15:55 +0530

    resolved conflict

--- main.c
+++ main.c
@@ -8,9 +8,6 @@ int main(){
    printf("Subtraction: %d\n", sub(5, 2));
    printf("Multiplication: %d\n", mul(3, 4));
    printf("Division: %d\n", div(10,2));
-<<<<<<< HEAD
    return 0;
    printf("hello everyone");
-=======
->>>>>>> 1bdb733a7946f4ec048a3d823aa6b89b0b2a17e9
 }
